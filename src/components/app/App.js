ce74d3364c849c719c02fcbbefc32cdeddf4920a
Casket <casket117@gmail.com>
2022-07-13 17:40:53 +0500

    end

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,13 @@
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import { MainPage, ComicsPage, SingleComicPage } from "../pages";
 import AppHeader from "../appHeader/AppHeader";
+import Spinner from "../spinner/Spinner";
 
-import Page404 from "../pages/404";
+const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 
 const App = () => {
 
@@ -13,12 +16,14 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Routes>
-                        <Route path="/" element={<MainPage/>}/>  
-                        <Route path ="/comics" element={<ComicsPage/>}/>
-                        <Route path ="/comics/:comicId" element={<SingleComicPage/>}/>
-                        <Route path ="*" element={<Page404/>}/>
-                    </Routes>
+                    <Suspense fallback={<Spinner/>}>
+                        <Routes>
+                            <Route path="/" element={<MainPage/>}/>  
+                            <Route path ="/comics" element={<ComicsPage/>}/>
+                            <Route path ="/comics/:comicId" element={<SingleComicPage/>}/>
+                            <Route path ="*" element={<Page404/>}/>
+                        </Routes>
+                    </Suspense>
                 </main>
             </div>
         </Router>
---
+++ src/components/pages/index.js
@@ -0,0 +1,8 @@
+import MainPage from "../pages/MainPage";
+import ComicsPage from "../pages/ComicsPage";
+import SingleComicPage from "./SingleComicPage";
+
+import Page404 from "./404";
+
+
+export {MainPage, ComicsPage, Page404, SingleComicPage};
